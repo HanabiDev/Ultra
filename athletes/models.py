d48aa0582cfe009d9fe7fbf5575e40ca1cb8677a
Juan Gómez <gomexa.10@gmail.com>
2016-09-19 07:53:57 -0500

    Updated athletes model

--- athletes/models.py
+++ athletes/models.py
@@ -23,11 +23,29 @@ class Athlete(models.Model):
         ('U', 'Universidad')
     )
 
+    GENRES = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino')
+    )
+
+    RH = (
+        ("AP", "A+"),
+        ("AM", "A-"),
+        ("ABP", "AB+"),
+        ("ABM", "AB-"),
+        ("BP", "B+"),
+        ("BM", "B-"),
+        ("OP", "O+"),
+        ("OM", "O-"),
+    )
+
     first_name = models.CharField(max_length=50, verbose_name=u'Nombres')
     last_name = models.CharField(max_length=50, verbose_name=u'Apellidos')
     document_type = models.CharField(max_length=1, verbose_name=u'Tipo de documento', choices=DNI_TYPES)
     document_number = models.CharField(max_length=30, verbose_name=u'Número de documento', unique=True)
     phone = models.CharField(max_length=15, verbose_name=u'Teléfono')
+    genre = models.CharField(max_length=1, verbose_name=u'Género', choices=GENRES)
+    rh = models.CharField(max_length=3, choices=RH, verbose_name=u'RH')
 
     province = models.ForeignKey(Province, verbose_name=u'Provincia')
     municipality = models.ForeignKey(Municipality, verbose_name=u'Municipio')
